503466e2b91a7c8e261b08d689645ba3c3a76c81
John Beech <github@mkv25.net>
2016-04-24 22:05:32 +0100

    Began to codeify area types

---
+++ planner/lib/areaTypes.js
@@ -0,0 +1,10 @@
+function list(areaTypes) {
+    var tiles = areaTypes.tiles;
+    var positions = areaTypes.positions;
+
+    return ['stub', tiles, positions];
+}
+
+module.exports = {
+    list
+};
--- planner/main.js
+++ planner/main.js
@@ -1,1 +1,10 @@
-console.log('Stub');
+var read = require('promise-path').read;
+var areaTypes = require('./lib/areaTypes');
+
+read('data/areaTypes.json', 'utf8')
+    .then(JSON.parse)
+    .then(areaTypes.list)
+    .then(console.log)
+    .catch((error) => {
+        console.log(error, error.stack);
+    });
