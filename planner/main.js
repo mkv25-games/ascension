52d513acf9fdef07153628033b7a6ebe2f2a474d
John Beech <john.beech@bbc.co.uk>
2016-04-25 17:31:16 +0100

    Write result out to replace input data

--- planner/main.js
+++ planner/main.js
@@ -1,13 +1,18 @@
 var read = require('promise-path').read;
+var write = require('promise-path').write;
 var areaTypes = require('./lib/areaTypes');
 
 read('data/areaTypes.json', 'utf8')
     .then(JSON.parse)
-    .then(areaTypes.add)
+    .then((areaData) => {
+        return areaTypes.add(areaData);
+    })
     .then((results) => {
         return JSON.stringify(results, null, 2);
     })
-    .then(console.log)
+    .then((areaData) => {
+        return write('data/areaTypes.json', areaData, 'utf8');
+    })
     .catch((error) => {
         console.log(error, error.stack);
     });
