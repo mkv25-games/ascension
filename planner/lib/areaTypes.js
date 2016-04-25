5729a43b1409e1c473320c32ff54b73e894deea0
John Beech <john.beech@bbc.co.uk>
2016-04-25 11:29:54 +0100

    Combined the list of possible positions

--- planner/lib/areaTypes.js
+++ planner/lib/areaTypes.js
@@ -1,8 +1,33 @@
 function list(areaTypes) {
-    var tiles = areaTypes.tiles;
+    var tiles = areaTypes.tiles.map((tile) => {
+        return tile.id;
+    });
     var positions = areaTypes.positions;
 
-    return ['stub', tiles, positions];
+    return positions.map((position) => {
+        return enhance(position, tiles);
+    });
+}
+
+function enhance(position, tiles) {
+    var spots = [position.id];
+
+    for(var i=0; i<position.typesRequired; i++) {
+        spots = combine(spots, tiles);
+    }
+
+    return spots;
+}
+
+function combine(spots, tiles) {
+    var result = [];
+    spots.forEach((id) => {
+        tiles.forEach((tile) => {
+            result.push(id + tile);
+        });
+    });
+
+    return result;
 }
 
 module.exports = {
