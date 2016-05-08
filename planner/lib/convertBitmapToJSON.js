e038742588e4c4ba4d610432bec631f1b1c5eb83
John Beech <github@mkv25.net>
2016-05-08 23:54:30 +0100

    Mapped the sample map input into json format

--- planner/lib/convertBitmapToJSON.js
+++ planner/lib/convertBitmapToJSON.js
@@ -1,5 +1,6 @@
 var getPixels = require('get-pixels');
 var createHistogram = require('./createHistogram');
+var tileTypes = require('../data/tileTypes');
 
 function convertBitmapToJSON(bitmapPath, callback) {
     getPixels(bitmapPath, function(err, pixels) {
@@ -21,12 +22,31 @@ function convertBitmapToJSON(bitmapPath, callback) {
             return pixel;
         };
 
+        var tilePixelMap = {};
+        tileTypes.tiles.forEach((tile) => {
+            tilePixelMap[tile.palette.code] = tile;
+        });
+
+        var shape = pixels.shape.slice();
+        var width = shape[0];
+        var height = shape[1];
+        var map = [];
+        for (var j = 0; j < width; j++) {
+            var row = [];
+            for (var i = 0; i < height; i++) {
+                pixel = pixels.getPixel(i, j);
+                row[i] = tilePixelMap[pixel.rgb].symbol;
+            }
+            map[j] = row.join(':');
+        }
+
         var histogram = createHistogram(pixels);
 
         console.log("Histogram", JSON.stringify(histogram, null, 2));
 
         (callback) ? callback({
-            histogram
+            histogram,
+            mapData: map
         }): null;
     });
 }
