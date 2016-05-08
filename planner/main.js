e0be432e8ed5ccaf6c5a2293e44e3552925ca8b7
John Beech <github@mkv25.net>
2016-05-08 10:23:03 +0100

    Split area render instructions into separate files

--- planner/main.js
+++ planner/main.js
@@ -21,6 +21,48 @@ function writeAreaCombinations() {
     var combinations = createAreaCombinations();
 
     write('data/areaCombinations.json', JSON.stringify(combinations, null, 2), 'utf8').catch(handleError);
+
+    var tileTypes = require('./data/tileTypes.json');
+    var tileSymbolMap = {};
+    tileTypes.tiles.forEach((tile) => {
+        tileSymbolMap[tile.symbol] = tile;
+    });
+
+    var renderAreaInstructions = combinations.combinations.map((combination) => {
+        var compass = combination.split('').map((tileSymbol) => {
+            return tileSymbolMap[tileSymbol].id;
+        });
+        return {
+            "asset": `areas/area-${combination}.png`,
+            "template": "/tiles/viewer.html",
+            "renderer": {
+                "$ref": "/data/renderers/mini-384x.json"
+            },
+            "data": {
+                "areaViewer": {
+                    "$ref": "/data/areaLayouts.json"
+                },
+                "NW": compass[0],
+                "NE": compass[1],
+                "SW": compass[2],
+                "SE": compass[3],
+                "zoom": "mini",
+                "layout": "beached"
+            }
+        };
+    });
+
+    var n = 0;
+    while(renderAreaInstructions.length > 0) {
+        var slice = [];
+        for(var i=0; i<10; i++) {
+            if(renderAreaInstructions.length > 0) {
+                slice.push(renderAreaInstructions.pop());
+            }
+        }
+        n++;
+        write(`instructions-generated/03-render-areas-slice-${n}.json`, JSON.stringify(slice, null, 2), 'utf8').catch(handleError);
+    }
 }
 
 writeAreaCombinations();
