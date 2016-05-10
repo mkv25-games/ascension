7e26a069c6d8130ef017bd898b128af589df716f
John Beech <github@mkv25.net>
2016-05-11 00:04:22 +0100

    Wired map data to map viewer

--- planner/templates/website/lib/map-data.js
+++ planner/templates/website/lib/map-data.js
@@ -1,1 +1,31 @@
-var mapData = {{json mapData}};
+var mapData = decode({{json mapData}});
+
+function decode(rawMapData) {
+    var j = 0;
+    var map = rawMapData.map(function(row) {
+        var row = row.split(':');
+        var i = 0;
+        row = row.map(function(symbol) {
+            var cell = {
+                symbol: symbol,
+                x: i,
+                y: j
+            };
+            i++;
+            return cell;
+        });
+        j++;
+        return row;
+    });
+
+    map.findTile = function(tile, xOffset, yOffset) {
+        var row = map[tile.y + yOffset];
+        if (row) {
+            return row[tile.x + xOffset] || row[tile.x] || tile;
+        } else {
+            return map[tile.y][tile.x + xOffset] || tile;
+        }
+    };
+
+    return map;
+}
