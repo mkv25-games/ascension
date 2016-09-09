86ce0dedfd4ed2ac3a6f7582fcb9548c654958dd
John Beech <github@mkv25.net>
2016-09-09 23:43:56 +0100

    Move PIXI aliases into separate file

--- viewer/script/start.js
+++ viewer/script/start.js
@@ -1,15 +1,5 @@
 const Viewer = (function() {
 
-    const TextureCache = PIXI.utils.TextureCache;
-    const Rectangle = PIXI.Rectangle;
-    const Loader = PIXI.loader;
-    const Resources = Loader.resources;
-    const Sprite = PIXI.Sprite;
-    const Container = PIXI.Container;
-    const ADR = PIXI.autoDetectRenderer;
-    const Graphics = PIXI.Graphics;
-    const Text = PIXI.Text;
-
     var renderer, ui, camera, stage, splash;
     var cameraInfoText;
     const options = {
