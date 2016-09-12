04b04276b9999baf10df8661d40c65d9b093b928
John Beech <github@mkv25.net>
2016-09-12 18:21:11 +0100

    Refactored touch controls to work better

--- viewer/script/ui/touch-controls.js
+++ viewer/script/ui/touch-controls.js
@@ -2,28 +2,49 @@ TouchControls = (() => {
     function create() {
         const controls = new Container();
 
+        const circle = createCircle();
+        const triangle = createTriangle();
+
+        registerTouchEvents(controls, circle, triangle);
+
+        controls.addChild(circle);
+        controls.addChild(triangle);
+
+        return controls;
+    }
+
+    function createCircle() {
         const circle = new Graphics();
         circle.lineStyle(8, 0xFFFFFF, 0.4);
         circle.beginFill(0xFFFFFF, 0.1);
         circle.drawCircle(0,0,100);
         circle.endFill();
-        circle.interactive = true;
+        return circle;
+    }
 
+    function createTriangle() {
+        const triangle = new Graphics();
+        triangle.endFill(0xFFFFFF, 1.0);
+        triangle.drawPolygon([-10, 10, 10, 10, 0, 0]);
+        triangle.endFill();
+        return triangle;
+    }
+
+    function registerTouchEvents(container, circle, triangle) {
         var timeout = 0;
-        circle.mousedown = circle.touchstart = (event) => {
+        container.interactive = true;
+        container.mousedown = container.touchstart = (event) => {
+            console.log('Controls mouse down');
             Tween.animate(circle, 1.0, {alpha: 1.0});
-            if(timeout > 0) {
-                clearTimeout(0);
+
+            clearTimeout(timeout);
+            container.mouseup = container.mouseupoutside = container.touchend = container.touchendoutside = (event) => {
+                console.log('Controls mouse up');
+                timeout = setTimeout(() => {
+                    Tween.animate(circle, 0.5, {alpha: 0.1});
+                }, 5000);
             }
-
-            timeout = setTimeout(() => {
-                Tween.animate(circle, 0.5, {alpha: 0.1});
-            }, 5000);
         };
-
-        controls.addChild(circle);
-
-        return controls;
     }
 
     return {
